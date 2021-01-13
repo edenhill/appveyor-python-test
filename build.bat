cbfc6d62655ab498b5d3b55476af11253e735b75
Magnus Edenhill <magnus@edenhill.se>
2021-01-13 15:19:56 +0100

    debug

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 SETLOCAL ENABLEEXTENSIONS
 
+echo on
+
+echo go
+
 set CIBW_BUILD=cp39-win_amd64
 set CIBW_SKIP=cp27-*
 set CIBW_BEFORE_BUILD=python -m pip install delvewheel==0.0.6
@@ -10,13 +14,26 @@ set DLL_DIR=%cd%\inst\runtimes\win-x64\native
 set LIB_DIRS=%cd%\inst\build\native\lib\win\x64\win-x64-Release\v120
 set CIBW_REPAIR_WHEEL_COMMAND=python -m delvewheel repair --add-path %cd%\inst\runtimes\win-x64\native -w {dest_dir} {wheel}
 
+echo go2
+
 set PATH=%PATH%;c:\Program Files\Git\bin\
+
+echo go3
 
 bash install-librdkafka.sh inst
 
+echo go4
+
 python -m pip install cibuildwheel==1.7.4
+
+echo go5
 
 python -m cibuildwheel --output-dir wheelhouse --platform windows
 
+echo go6
+
 dir wheelhouse
+
+echo go7
+
 
