0fddf8247906c2804b0032ff2db605771ba0b422
Magnus Edenhill <magnus@edenhill.se>
2021-01-13 16:06:21 +0100

    x86 and x64 builds

--- build.bat
+++ build.bat
@@ -1,19 +1,23 @@
 SETLOCAL ENABLEEXTENSIONS
 
+rem x86 or x64
+set ARCH=%1
+rem win32 or win_amd64
+set BW_ARCH=%2
+
 echo on
 
 echo go
 
-set CIBW_BUILD=cp39-win_amd64
-set CIBW_SKIP=cp27-*
+set CIBW_BUILD=cp27-%BW_ARCH% cp36-%BW_ARCH% cp37-%BW_ARCH% cp38-%BW_ARCH% cp39-%BW_WARCH%
 set CIBW_BEFORE_BUILD=python -m pip install delvewheel==0.0.6
 set xCIBW_TEST_COMMAND={project}\test.bat {project}
 set CIBW_TEST_COMMAND=python {project}\test.py
 set CIBW_BUILD_VERBOSITY=3
 set INCLUDE_DIRS=%cd%\inst\build\native\include
-set DLL_DIR=%cd%\inst\runtimes\win-x64\native
-set LIB_DIRS=%cd%\inst\build\native\lib\win\x64\win-x64-Release\v120
-set CIBW_REPAIR_WHEEL_COMMAND=python -m delvewheel repair --add-path %cd%\inst\runtimes\win-x64\native -w {dest_dir} {wheel}
+set DLL_DIR=%cd%\inst\runtimes\win-%ARCH%\native
+set LIB_DIRS=%cd%\inst\build\native\lib\win\%ARCH%\win-%ARCH%-Release\v120
+set CIBW_REPAIR_WHEEL_COMMAND=python -m delvewheel repair --add-path %DLL_DIR% -w {dest_dir} {wheel}
 
 echo go2
 
--- install-librdkafka.sh
+++ install-librdkafka.sh
@@ -12,7 +12,7 @@ fi
 mkdir -p "$DEST"
 cd "$DEST"
 
-curl -L -o lrk.zip https://www.nuget.org/api/v2/package/librdkafka.redist/1.6.0-RC1
+curl -L -o lrk.zip https://www.nuget.org/api/v2/package/librdkafka.redist/${LIBRDKAFKA_VERSION}
 
 unzip lrk.zip
 
