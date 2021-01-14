a493c275a8a1106ef5f6bbd5e1e432aebd635e28
Magnus Edenhill <magnus@edenhill.se>
2021-01-14 17:36:32 +0100

    prefer the bestest

--- install-librdkafka.sh
+++ install-librdkafka.sh
@@ -15,3 +15,7 @@ cd "$DEST"
 curl -L -o lrk.zip https://www.nuget.org/api/v2/package/librdkafka.redist/${LIBRDKAFKA_VERSION}
 
 unzip lrk.zip
+
+# Copy the librdkafka build with least dependencies to librdkafka.so
+cp -v runtimes/linux-x64/native/{debian9-,}librdkafka.so
+
